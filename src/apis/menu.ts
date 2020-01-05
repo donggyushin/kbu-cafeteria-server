bd1f43de156b94403089ae06573e9a43ad36157e
shin donggyu <donggyu9410@gmail.com>
2020-01-05 19:07:26 +0900

    Fet menus based on specific date

--- src/apis/menu.ts
+++ src/apis/menu.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-import { GetMenusOnAMonthly, PutNewMenu } from '../controllers/menu'
+import { GetMenusOnAMonthly, PutNewMenu, getMenusBasedOnSpecificDate } from '../controllers/menu'
 const router = express.Router()
 
 // 해당 연과 달에 해당하는 모든 menu 오브젝트들을 반환해주는 api
+router.get('/menus/:date1/:date2', getMenusBasedOnSpecificDate)
 router.get('/:year/:month', GetMenusOnAMonthly)
+
 router.put('', PutNewMenu)
 
 export default router
--- src/controllers/menu.ts
+++ src/controllers/menu.ts
@@ -1,8 +1,115 @@
 import { Request, Response } from 'express'
-import { IMenu } from '../types'
+import { IMenu, IDinner, ILunch } from '../types'
 import MenuModel from '../models/menu'
 import dotenv from 'dotenv'
 dotenv.config()
+
+interface IGetMenusBasedOnSpecificDateParams {
+    date1?: string
+    date2?: string
+}
+
+interface IGetMenusBasedOnSpecificDateResponse {
+    ok: boolean
+    error: string
+    menus: IMenu[]
+}
+
+export const getMenusBasedOnSpecificDate = async (req: Request, res: Response) => {
+
+    const {
+        date2,
+        date1
+    }: IGetMenusBasedOnSpecificDateParams = req.params
+
+    let result: IGetMenusBasedOnSpecificDateResponse = {
+        ok: false,
+        error: null,
+        menus: []
+    }
+    const menusList: IMenu[] = []
+    const diff: number = parseInt(date2) - parseInt(date1)
+    // diff 는 음수여도 안되고, 5일 이상 차이나도 안됨. diff는 0 부터 5일 차이까지. 
+    // 우선 5일이 miliseconds 로 얼마인지 구해보자. 
+    // 5일은 miliseconds 로 345600000 이다. 
+
+    if (diff >= 0 && diff <= 345600000) {
+        const parsedDate1: Date = new Date(parseInt(date1))
+        const days: number = diff / 86400000
+        const startYear: number = parsedDate1.getFullYear()
+        const startMonth: number = parsedDate1.getMonth()
+        const startDay: number = parsedDate1.getDate()
+        const numberOfDaysOfTheStartMonth: number = 32 - new Date(startYear, startMonth, 32).getDate()
+
+
+        let year: number = startYear
+        let month: number = startMonth
+        let day: number = startDay
+
+        try {
+            for (let i = 0; i <= days; i++) {
+                const menu: IMenu = await MenuModel.findOne({
+                    year,
+                    month,
+                    day
+                })
+
+                if (menu) {
+                    menusList.push(menu)
+                } else {
+                    const dinner = {
+                        menus: []
+                    }
+                    const lunch = {
+                        menus: []
+                    }
+
+                    const newMenu: IMenu = await new MenuModel({
+                        year,
+                        month,
+                        day,
+                        dinner,
+                        lunch
+                    })
+                    await newMenu.save()
+                    menusList.push(newMenu)
+                }
+
+                if (day === numberOfDaysOfTheStartMonth) {
+                    day = 1
+                    if (month === 11) {
+                        month = 0
+                        year = year + 1
+                    } else {
+                        month = month + 1
+                    }
+                } else {
+                    day = day + 1
+                }
+
+
+            }
+
+            result.ok = true
+            result.menus = menusList
+            res.json(result)
+            return
+        } catch (err) {
+            console.error(`Error occured at [${__dirname}]:${err.message}`)
+            result.ok = false
+            result.error = "알수없는 이유로 인해서 메뉴들을 불러오지 못하였습니다. 관리자에게 문의해주세요. 01090411019"
+            res.json(result)
+            return
+        }
+
+    } else {
+        result.error = '날짜 입력 양식이 잘못되었습니다. 최소 1일, 최대 5일 까지의 범위만 입력해주세요.'
+        res.json(result)
+        return
+    }
+}
+
+
 
 interface IPutNewMenuResponse {
     ok: boolean
