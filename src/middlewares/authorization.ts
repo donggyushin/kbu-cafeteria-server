0a724b9f82c7f528d3510f8c03d8da8adcfbeeb7
shin donggyu <donggyu9410@gmail.com>
2020-01-09 16:43:03 +0900

    Added middleware module that checks that client has appropriate cookie or not

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from 'express'
+import { decodeFooJsonwebtoken } from '../utils/jsonwebtoken'
+import { IUser } from '../types'
+import UserModel from '../models/user'
+
+
+declare module 'http' {
+    interface IncomingHttpHeaders {
+        "authentication"?: string
+    }
+}
+
+interface ICheckUserAuthHeaderProps {
+    authentication?: string
+}
+
+interface IResponseType {
+    ok: boolean
+    error: string
+}
+
+export const checkUserAuth = async (req: Request, res: Response, next: NextFunction) => {
+    const { authentication }: ICheckUserAuthHeaderProps = req.headers
+    let result: IResponseType = {
+        ok: false,
+        error: null
+    }
+    if (authentication) {
+        const userId: string = decodeFooJsonwebtoken(authentication)
+        try {
+            const user: IUser = await UserModel.findById(userId)
+            if (user) {
+                next()
+                return
+            } else {
+                result.error = `유효한 Cookie 가 아닙니다. 관리자에게 문의해주세요 01090411019`
+                res.json(result)
+                return
+            }
+        } catch (err) {
+            result.error = `Internal error`
+            res.status(500)
+            res.json(result)
+            return
+        }
+
+    } else {
+        result.error = `Cookie 값이 확인되지 않았습니다. 관리자에게 문의해주세요 01090411019`
+        res.json(result)
+        return
+    }
+}
