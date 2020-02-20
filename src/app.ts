e6ff9746c1e05b3f90b97034ae1587cfbd0fc804
shin donggyu <donggyu9410@gmail.com>
2020-02-21 02:56:21 +0900

    Work for moving server. http protocol

--- src/app.ts
+++ src/app.ts
@@ -9,19 +9,19 @@ dotenv.config()
 
 let env = process.env.NODE_ENV || 'dev';
 
-const key = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/privkey.pem', 'utf8')
-const cert = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/cert.pem', 'utf8')
-const chain = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/chain.pem', 'utf8')
+// const key = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/privkey.pem', 'utf8')
+// const cert = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/cert.pem', 'utf8')
+// const chain = fs.readFileSync('/etc/letsencrypt/live/kbucard.com/chain.pem', 'utf8')
 
-const credentials = {
-    key,
-    cert,
-    ca: chain
-}
+// const credentials = {
+//     key,
+//     cert,
+//     ca: chain
+// }
 
 const app: Application = express()
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 const PORT = process.env.PORT
 
 app.use(cors())
@@ -32,5 +32,6 @@ app.use('/api', API)
 if (env === 'dev') {
     httpServer.listen(PORT, () => console.log(`kbu-cafeteria-server listening on port ${PORT}`))
 } else {
-    httpsServer.listen(PORT, () => console.log(`kbu-cafeteria-server listening on port ${PORT}`))
+    // httpsServer.listen(PORT, () => console.log(`kbu-cafeteria-server listening on port ${PORT}`))
+    httpServer.listen(PORT, () => console.log(`kbu-cafeteria-server listening on port ${PORT}`))
 }
